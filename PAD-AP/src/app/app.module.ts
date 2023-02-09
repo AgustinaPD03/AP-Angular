92a5e501a1c1e5b884a987ca295396618aa29e0b
AgustinaPD03 <kagusdelgado@gmail.com>
2023-02-09 15:29:07 -0300

    09/02/2023
    Se creo una nueva sección SObre-Mi remplazando la anterior, la cual tiene dentro los componentes de "Descripción", "Estudios y Experiencias Laborales" y "Habilidades"

--- PAD-AP/src/app/app.module.ts
+++ PAD-AP/src/app/app.module.ts
@@ -5,22 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Componentes/navbar/navbar.component';
 import { InicioComponent } from './Componentes/inicio/inicio.component';
-import { DescComponent } from './Componentes/Sobre-Mi/desc/desc.component';
-import { HabyExpComponent } from './Componentes/Sobre-Mi/haby-exp/haby-exp.component';
-import { HabComponent } from './Componentes/Sobre-Mi/hab/hab.component';
 import { ProyectosComponent } from './Componentes/proyectos/proyectos.component';
 import { FooterComponent } from './Componentes/footer/footer.component';
+import { SobreMi2Component } from './Componentes/sobre-mi2/sobre-mi2.component';
+import { HabyexpComponent } from './Componentes/sobre-mi2/habyexp/habyexp.component';
+import { DesscpComponent } from './Componentes/sobre-mi2/desscp/desscp.component';
+import { HabilidadesComponent } from './Componentes/sobre-mi2/habilidades/habilidades.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     InicioComponent,
-    DescComponent,
-    HabyExpComponent,
-    HabComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    SobreMi2Component,
+    HabyexpComponent,
+    DesscpComponent,
+    HabilidadesComponent
   ],
   imports: [
     BrowserModule,
