09449bd3b1a270a548a23cea4de5bda118da5337
AgustinaPD03 <kagusdelgado@gmail.com>
2023-02-14 19:19:51 -0300

    14/02/2023
    Se agregan los compoenetes "login" y "Regsiter"

---
+++ PAD-AP/src/app/Componentes/log-yreg/log-yreg.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-yreg',
+  templateUrl: './log-yreg.component.html',
+  styleUrls: ['./log-yreg.component.css']
+})
+export class LogYRegComponent {
+
+}
