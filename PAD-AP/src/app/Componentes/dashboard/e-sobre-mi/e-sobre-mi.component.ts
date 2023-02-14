b2d0db7fe80b1fa6a001e1640e5d1183911da319
AgustinaPD03 <kagusdelgado@gmail.com>
2023-02-14 19:07:44 -0300

    14/02/2023
    Se agrega el Dashboar y esta dividido en secciones de la misma forma que el index

---
+++ PAD-AP/src/app/Componentes/dashboard/e-sobre-mi/e-sobre-mi.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-e-sobre-mi',
+  templateUrl: './e-sobre-mi.component.html',
+  styleUrls: ['./e-sobre-mi.component.css']
+})
+export class ESobreMiComponent {
+
+}
